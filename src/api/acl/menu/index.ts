a32642fbcfd385bec1a4f993def92e6b71f4edb2
liuhuazheng <erka66952@gmail.com>
2025-09-13 15:37:12 +0800

    修改接口

--- src/api/acl/menu/index.ts
+++ src/api/acl/menu/index.ts
@@ -1,5 +1,5 @@
 import request from "@/utils/request";
-import type { PermisstionResponseData, MenuParams } from './type';
+import type { PermisstionResponseData, MenuParams }  from './type';
 //枚举地址
 enum API {
     //获取全部菜单与按钮的标识数据
